07b85d756c8c38b4b481313b060bbb86320bc54a
maugallo <maugallok@gmail.com>
2025-05-07 21:49:19 -0300

    First commit

---
+++ src/test/java/com/maugallo/munify_backend/MunifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maugallo.munify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
